d5ea076efda645700e2a1203f44c4284cc0daade
Vinz <vinzent.beer@gmail.com>
2026-05-18 12:31:30 +0200

    change block size to use 2d block addressing

--- julia_cuda/src/kernel_gpu.cu
+++ julia_cuda/src/kernel_gpu.cu
@@ -5,7 +5,8 @@ extern int global_block_x, global_block_y;
 
 __global__ void julia_kernel_gpu(float *julia_set, Complex c, float scale, int res_x, int res_y, int max_iter, float max_mag, float x_scale, float y_scale){
     //we no longer pass in the pixel coords but calculate them from the thread parameters
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int local_tid = threadIdx.y * blockDim.x + threadIdx.x;                                                                                                                                    
+    int tid = blockIdx.x * (blockDim.x * blockDim.y) + local_tid;
     int total_pixels = res_x * res_y;
     //guard against too many thread launches 
     if (tid >= total_pixels) return;
@@ -35,9 +36,9 @@ void julia_kernel(float *julia_set, Complex c, float scale, int res_x, int res_y
 
     int total_pixels = res_x * res_y;
     // compute a good default block size
-    int block_size = 256; //multiple of 32 (warp size) 
+    dim3 block(32,8); //8 warps for a 2d block
     if (global_block_x > 0) {
-        block_size = global_block_x;
+        block = dim3(global_block_x, global_block_y);
     }
 
     //array size 
@@ -45,9 +46,9 @@ void julia_kernel(float *julia_set, Complex c, float scale, int res_x, int res_y
     float *d_julia_set;
     cudaMalloc(&d_julia_set, mem_size);
 
-    int grid_size = (total_pixels + block_size - 1) / block_size; //round up division
+    int grid_size = (total_pixels + block.x * block.y - 1) / (block.x * block.y); //round up division
 
-    julia_kernel_gpu<<<grid_size,block_size>>>(d_julia_set, c, scale, res_x, res_y, max_iter, max_mag, x_scale, y_scale);
+    julia_kernel_gpu<<<grid_size,block>>>(d_julia_set, c, scale, res_x, res_y, max_iter, max_mag, x_scale, y_scale);
     cudaDeviceSynchronize();
     cudaMemcpy(julia_set, d_julia_set, mem_size, cudaMemcpyDeviceToHost);
     cudaFree(d_julia_set);
