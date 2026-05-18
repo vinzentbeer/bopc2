f68df1ca2b7755bf78512921cc575c3ea9e2fee9
Vinz <vinzent.beer@gmail.com>
2026-05-18 11:46:59 +0200

    cudamalloc and free with correct (?) grid sizes

--- julia_cuda/src/kernel_gpu.cu
+++ julia_cuda/src/kernel_gpu.cu
@@ -3,8 +3,35 @@
 
 extern int global_block_x, global_block_y;
 
+__global__ void julia_kernel_gpu(float *julia_set, Complex c, float scale, int res_x, int res_y, int max_iter, float max_mag, float x_scale, float y_scale){
+    //we no longer pass in the pixel coords but calculate them from the thread parameters
+    tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int total_pixels = res_x * res_y;
+    //guard against too many thread launches 
+    if (tid >= total_pixels) return;
+
+    int x = tid / res_y; 
+    int y = tid % res_y;
+}   
+
+
+
 void julia_kernel(float *julia_set, Complex c, float scale, int res_x, int res_y, int max_iter, float max_mag, float x_scale, float y_scale) {
 
+    int total_pixels = res_x * res_y;
     // compute a good default block size
+    int block_size = 256; //multiple of 32 (warp size) 
+
+
+    //array size 
+    size_t mem_size = total_pixels * sizeof(float);
+    float *d_julia_set;
+    cudaMalloc(&d_julia_set, mem_size);
+
+    int grid_size = (total_pixels + block_size - 1) / block_size; //round up division
+
+    julia_kernel_gpu<<<grid_size,block_size>>>(d_julia_set, c, scale, res_x, res_y, max_iter, max_mag, x_scale, y_scale);
+    cudaMemcpy(julia_set, d_julia_set, mem_size, cudaMemcpyDeviceToHost);
+    cudaFree(d_julia_set);
 
 }
