aca62e7e0a2b88bc6542455ae506bf2705ffd180
Vinz <vinzent.beer@gmail.com>
2026-05-18 10:59:47 +0200

    add more detail

--- julia_cuda/src/cuda_foobar.cu
+++ julia_cuda/src/cuda_foobar.cu
@@ -4,12 +4,13 @@ __global__ void i_print_my_tid()
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     printf("Hello from thread %d!\n", tid);
+    printf("  blockIdx.x = %d, blockDim.x = %d, threadIdx.x = %d\n", blockIdx.x, blockDim.x, threadIdx.x);
 }
 
 int main ()
 {
     // 2 blocks of 4 threads each 
-    i_print_my_tid<<<2, 4>>>();
+    i_print_my_tid<<<3, 7>>>();
     cudaDeviceSynchronize(); 
     return 0;
 }
